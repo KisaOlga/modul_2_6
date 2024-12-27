cba5d398a2c9e84ce79c33ef0e092ea40be87f37
Ольга <olgagladunova1@gmail.com>
2024-12-27 18:10:12 +0300

    Add files via upload
    Дополнительное практическое задание по модулю: "Основные операторы"
    Цель: Применить знания полученные в модуле, решив задачу повышенного уровня сложности
    Задание "Слишком древний шифр":
    Вы отправились в путешествие на необитаемый остров и конечно же в очередной вылазке в джунгли вы попали в ловушку местному племени (да-да, классика "Индиана Джонса").
    К вашему удивлению, в племени были неплохие математики и по совместительству фантазёры.
    Вы поняли это, когда после долгих блужданий перед вами появились ворота (выход из ловушки) с двумя каменными вставками для чисел.
    В первом поле камни с числом менялись постоянно (от 3 до 20) случайным образом, а второе было всегда пустым.
    К вашему счастью рядом с менее успешными и уже неговорящими путешественниками находился папирус, где были написаны правила для решения этого "ребуса". (Как жаль, что они поняли это так поздно :( ).
    Во вторую вставку нужно было написать те пары чисел друг за другом, чтобы число из первой вставки было кратно(делилось без остатка) сумме их значений.

---
+++ modul_2_6.py
@@ -0,0 +1,15 @@
+number = int(input('введитете число от 3 до 20: '))
+
+def get_password(number):
+    password = ' '
+    for i in range(1, number):
+        for j in range(2, number):
+            if j <= i:
+                continue
+            if number % (i + j) == 0:
+                password += str(i) + str(j)
+    return password
+
+result = get_password(number)
+print('Пароль:', result)
+
